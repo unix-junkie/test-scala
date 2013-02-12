316bf231a3add8cdc382e3b8d77023b9128dcfd4
Andrew ``Bass'' Shcheglov <andrewbass@gmail.com>
2013-02-13 01:16:19 +0400

    MINOR: SVG export enabled

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -3,14 +3,20 @@
  */
 package com.example
 
+import java.awt.Rectangle
 import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
 import java.util.Date
 
+import org.apache.batik.dom.GenericDOMImplementation
+import org.apache.batik.svggen.SVGGraphics2D
 import org.jfree.chart.ChartFactory
 import org.jfree.chart.ChartUtilities
 import org.jfree.data.time.Day
 import org.jfree.data.time.TimeSeries
 import org.jfree.data.time.TimeSeriesCollection
+import org.w3c.dom.DOMImplementation
 
 /**
  * @author ashcheglov
@@ -31,5 +37,15 @@ object Main {
  
 		val chart = ChartFactory.createTimeSeriesChart("User Participation Chart", "Day", "Number Of Users", dataset, true, true, false)
 		ChartUtilities.saveChartAsJPEG(new File("test.jpeg"), chart, 800, 600);
+		
+		val domImpl: DOMImplementation = GenericDOMImplementation.getDOMImplementation()
+		val document = domImpl.createDocument(null, "svg", null)
+		val svgGenerator = new SVGGraphics2D(document)
+		chart.draw(svgGenerator, new Rectangle(800, 600))
+		
+		val out = new OutputStreamWriter(new FileOutputStream("test.svg"), "UTF-8")
+		svgGenerator.stream(out, true)
+		out.flush()
+		out.close()
 	}
 }
